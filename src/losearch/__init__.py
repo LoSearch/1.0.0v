af42e9677f631ac0c5614281528fbf88cc37b75f
MadricTeam <locsearch1@gmail.com>
2025-08-30 06:09:49 +1300

    Update __init__.py

--- src/losearch/__init__.py
+++ src/losearch/__init__.py
@@ -54,7 +54,7 @@ def analyze_text(text: str):
     engine = LoSearch()
     return engine.analyze_text(text)
 
-__version__ = "1.0.0"
+__version__ = "0.1.2"
 __all__ = [
     'LoSearch',
     'TextProcessor',
